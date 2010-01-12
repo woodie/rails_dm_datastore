880bd626dc572eb943ca456846de85c58e7f1bbb
John Woodell <woodie@netpress.com>
2010-01-12 10:25:33 -0800

    fix the defaults for standard DataMapper types

--- generators/dm_model/templates/model.rb
+++ generators/dm_model/templates/model.rb
@@ -1,10 +1,10 @@
 <% max = 2
+   presets = {'Text' => ':lazy => false', 'String' => ':length => 500'}
    reserved_dm_names = DataMapper::Resource.instance_methods +
                        DataMapper::Resource.private_instance_methods 
    datastore_types = %w(AncestorKey BigDecimal Blob Boolean ByteString
        Category Class Date DateTime Email Float Integer Key Link Object 
        PhoneNumber PostalAddres Rating String Text Time TrueClass) -%>
-   presets = {'Text' => ':lazy => false', 'String' => ':length => 500'}
 <% Array(attributes).each do |attribute|
      if reserved_dm_names.include? attribute.name
        raise "reserved property name '#{attribute.name}'"
@@ -20,7 +20,7 @@ class <%= class_name %>
 <% Array(attributes).each do |attribute|
      klass = attribute.type.to_s.classify.to_s
      klass += 's' if klass.eql? 'PostalAddres' # classify bug
-     more = presets.has_key? klass ? ", #{presets[klass]}" : ''
+     more = presets.has_key?(klass) ? ", #{presets[klass]}" : ''
      pad = max - attribute.name.size
      rad = 13 - klass.size
      %>  property :<%= attribute.name %>, <%= " " * pad
