b21cb35c41a60e94e02bdf7903fafcac40bd0d06
joshsmoore <joshsmoore@gmail.com>
2010-02-08 13:54:12 +0800

    Fixes for Date and Time conversion from Strings provided provided by John Woodell

--- lib/rails_dm_datastore.rb
+++ lib/rails_dm_datastore.rb
@@ -79,3 +79,44 @@ module ActionView
   
   end
 end
+
+
+# provided by John Woodell mando.woodie@gmail.com to fix problems with date conversion
+module ActiveSupport #:nodoc:
+  module CoreExtensions #:nodoc:
+    module String #:nodoc:
+      # Converting strings to other objects
+      module Conversions
+        # 'a'.ord == 'a'[0] for Ruby 1.9 forward compatibility.
+        def ord
+          self[0]
+        end if RUBY_VERSION < '1.9'
+
+        # Form can be either :utc (default) or :local.
+        def to_time(form = :utc)
+          begin
+            ::Time.send("#{form}_time", *::Date._parse(self, false).values_at(:year, :mon, :mday, :hour, :min, :sec).map { |arg| arg || 0 })
+          rescue
+            nil
+          end
+        end
+
+        def to_date
+          begin
+            ::Date.new(*::Date._parse(self, false).values_at(:year, :mon, :mday))
+          rescue
+            nil
+          end
+        end
+
+        def to_datetime
+          begin
+            ::DateTime.civil(*::Date._parse(self, false).values_at(:year, :mon, :mday, :hour, :min, :sec).map { |arg| arg || 0 })
+          rescue
+            nil
+          end
+        end
+      end
+    end
+  end
+end
