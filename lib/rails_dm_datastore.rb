abad83c385bcc4142e925238ee38ca4364716f47
John Woodell <woodie@netpress.com>
2010-02-08 09:10:23 -0800

    clean up the main require and add comments

--- lib/rails_dm_datastore.rb
+++ lib/rails_dm_datastore.rb
@@ -4,9 +4,9 @@ require 'dm-ar-finders'
 require 'dm-timestamps'
 require 'dm-validations'
 DataMapper.setup(:default, "appengine://auto")
-#DataMapper.setup(:ephemeral, "in_memory::")
 
-#convert the date from the date picker to an actual date class that the datastore can actually store
+# Convert the values from the date and time picker
+# to a date or time class that the datastore can accept
 def fix_date(hash, property, type)
   total_attributes = 0
   if Date == type
@@ -14,7 +14,6 @@ def fix_date(hash, property, type)
   else
     total_attributes = 5
   end
-  
   time_string = ""
   1.upto(total_attributes) do |n|
     if n == 1
@@ -28,7 +27,6 @@ def fix_date(hash, property, type)
     end
     hash.delete :"#{property}(#{n}i)"
   end
-
   hash[property] = type.parse(time_string).send("to_#{type.to_s.downcase}")
   hash
 end
@@ -43,12 +41,12 @@ module DataMapper
     # avoid NoMethodError
     def update_attributes(*args); update(*args); end
     
-    #make sure that all properties of the model that have to do with date or time are converted run through
-    # the fix_date converter
+    # make sure that all properties of the model that have to do with
+    # date or time are converted run through the fix_date converter
     def attributes=(attributes)
       self.class.properties.each do |t| 
-        if !(t.name.to_s =~ /.*_at/) && (t.type.to_s =~ /Date/ || t.type.to_s =~ /Time/ ) && 
-          attributes.include?("#{t.name.to_s}(1i)")
+        if !(t.name.to_s =~ /.*_at/) && (t.type.to_s =~ /Date|Time/ ) && 
+            attributes.include?("#{t.name.to_s}(1i)")
           fix_date(attributes, t.name.to_s, t.type) 
         end
       end 
@@ -60,13 +58,11 @@ end
 # DataMapper::Validate
 class Dictionary; alias count length; end
  
- 
- #Override Extlib::Hook::ClassMethods.inline_call to check in the given weak reference
-
+# Override Extlib::Hook::ClassMethods.inline_call
+# to check in the given weak reference
 module LocalObjectSpace
   def self.extended(klass)
     (class << klass; self;end).send :attr_accessor, :hook_scopes
-   
     klass.hook_scopes = []
   end
   
@@ -75,21 +71,16 @@ module LocalObjectSpace
       return object if object.object_id == object_id
     end
   end
-  
 end
- 
+
+# Fix LocalObjectSpace hooks
 module Extlib
   module Hook
     module ClassMethods
-      
       extend LocalObjectSpace
-        
-      #end
-      
       def inline_call(method_info, scope)
         Extlib::Hook::ClassMethods.hook_scopes << method_info[:from]
         name = method_info[:name]
-        
         if scope == :instance
           args = method_defined?(name) && instance_method(name).arity != 0 ? '*args' : ''
           %(#{name}(#{args}) if self.class <= Extlib::Hook::ClassMethods.object_by_id(#{method_info[:from].object_id}))
@@ -102,12 +93,12 @@ module Extlib
   end
 end
 
-# makes the shorthand <%= render @posts %> work for collections of DataMapper objects
+# makes the shorthand <%= render @posts %> work
+# for collections of DataMapper objects
 module ActionView
   module Partials
   alias :render_partial_orig :render_partial
   private
-  
     def render_partial(options = {})
       if DataMapper::Collection === options[:partial]
         collection = options[:partial]
@@ -117,12 +108,11 @@ module ActionView
         render_partial_orig(options)      
       end
     end
-  
   end
 end
 
-
-# provided by John Woodell mando.woodie@gmail.com to fix problems with date conversion
+# set a nil date or time when a date cannot be parced
+# to avoid exception by ruby via to_date and to_time
 module ActiveSupport #:nodoc:
   module CoreExtensions #:nodoc:
     module String #:nodoc:
@@ -132,27 +122,29 @@ module ActiveSupport #:nodoc:
         def ord
           self[0]
         end if RUBY_VERSION < '1.9'
-
         # Form can be either :utc (default) or :local.
         def to_time(form = :utc)
           begin
-            ::Time.send("#{form}_time", *::Date._parse(self, false).values_at(:year, :mon, :mday, :hour, :min, :sec).map { |arg| arg || 0 })
+            ::Time.send("#{form}_time", *::Date._parse(self, false).
+                values_at(:year, :mon, :mday, :hour, :min, :sec).
+                map { |arg| arg || 0 })
           rescue
             nil
           end
         end
-
         def to_date
           begin
-            ::Date.new(*::Date._parse(self, false).values_at(:year, :mon, :mday))
+            ::Date.new(*::Date._parse(self, false).
+                values_at(:year, :mon, :mday))
           rescue
             nil
           end
         end
-
         def to_datetime
           begin
-            ::DateTime.civil(*::Date._parse(self, false).values_at(:year, :mon, :mday, :hour, :min, :sec).map { |arg| arg || 0 })
+            ::DateTime.civil(*::Date._parse(self, false).
+                values_at(:year, :mon, :mday, :hour, :min, :sec).
+                map { |arg| arg || 0 })
           rescue
             nil
           end
